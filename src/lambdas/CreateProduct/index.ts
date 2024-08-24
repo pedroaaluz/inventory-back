7b5569e1ef0977c95618f00fbf03a40fa2cedde1
pedroaaluz <pedroaluz137@gmail.com>
2024-08-24 12:58:09 -0300

    Revert "delete files"
    This reverts commit c408bd3b9ba3e439806282911b759f6091264c56.

---
+++ src/lambdas/CreateProduct/index.ts
@@ -0,0 +1,38 @@
+import {makeCreateNewProductController} from './factories/makeCreateProduct';
+import {HttpFn} from '../../common/types/lambdasTypes';
+import {requestSchema, responseSchema} from './schema';
+import {z} from 'zod';
+import handler from '../../common/middlewares/handler';
+import {zodValidatorMiddleware} from '../../common/middlewares/zodValidator';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import httpBodyNormalize from '../../common/middlewares/httpBodyNormalize';
+
+/**
+ * @description Lambda responsible for creating a new product and associating it with its supplier, category,
+ * and creating an ADD_TO_STOCK movement.
+ * @invoke sls invoke local -f CreateProduct -p src/lambdas/createProduct/mock.json
+ */
+
+const fn: HttpFn<
+  z.infer<typeof requestSchema>,
+  z.infer<(typeof responseSchema)['200']>
+> = async event => {
+  try {
+    const controller = makeCreateNewProductController();
+
+    const response = await controller.exec(event);
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const bootstrap = handler(fn, [
+  httpEventNormalizer(),
+  httpBodyNormalize(),
+  zodValidatorMiddleware({requestSchema, responseSchema}),
+  httpErrorHandler(),
+]);
---
+++ src/lambdas/GetProduct/domain/GetProductUseCase.ts
@@ -0,0 +1,29 @@
+import {UseCase} from '../../../common/interfaces';
+import {GetProductRepository} from '../../../common/repositories/product/getProductRepository';
+import type {Product} from '@prisma/client';
+
+export class GetProductUseCase
+  implements
+    UseCase<
+      string,
+      {product: Product; supplierId: string[]; category: string[]} | null
+    >
+{
+  constructor(private readonly getProductRepository: GetProductRepository) {}
+
+  async exec(input: string) {
+    const productDTO = {
+      productId: input,
+    };
+
+    console.log('Product', productDTO);
+
+    const product = await this.getProductRepository.exec(productDTO.productId);
+
+    if (product == null) {
+      return null;
+    }
+
+    return product;
+  }
+}
