d8ac68b29e78bf1df71e7143f48ea53b26e82fe0
pedroaaluz <pedroaluz137@gmail.com>
2024-09-08 13:21:11 -0300

    feat: Remove unused files and refactor getProduct feature

--- src/lambdas/UpdateProduct/ports/controllers/UpdateProductController.ts
+++ src/lambdas/UpdateProduct/ports/controllers/UpdateProductController.ts
@@ -1,73 +1,23 @@
+import {APIGatewayEvent} from 'aws-lambda';
 import {Controller} from '../../../../common/interfaces';
 import {UpdateProductUseCase} from '../../domain/UpdateProductUseCase';
-import {requestSchema, responseSchema} from '../../schema';
-import { HttpEvent, HttpResponse } from '../../../../common/types/lambdasTypes';
-import {z} from 'zod';
 
-export class UpdateProductController 
-  implements
-    Controller<
-      HttpEvent<z.infer<typeof requestSchema>>,
-      Promise<
-        | HttpResponse<z.infer<(typeof responseSchema)['200']>>
-        | HttpResponse<z.infer<(typeof responseSchema)['404']>>
-      > 
-    >
-{
-  constructor(private readonly updateProductUseCase: UpdateProductUseCase) {}
+export class UpdateProductController implements Controller<APIGatewayEvent> {
+  constructor(private readonly createNewProductUseCase: UpdateProductUseCase) {}
 
-  async exec(
-    event: HttpEvent<z.infer<typeof requestSchema>>,
-  ): Promise<
-    | HttpResponse<z.infer<(typeof responseSchema)['200']>>
-    | HttpResponse<z.infer<(typeof responseSchema)['404']>>
-  > {
+  async exec(event: APIGatewayEvent) {
     try {
-      const id = event.body.id;
+      const body = JSON.parse(event.body);
 
-      if (id == null) {
-        return {
-          statusCode: 400,
-          body: {
-            message: 'Id para atualização não informado',
-          },
-        };
-      }
-
-      const result = await this.updateProductUseCase.exec(event);
-
-      if (result === null) {
-        return {
-          statusCode: 404,
-          body: {
-            message: 'Product not found',
-          },
-        };
-      }
+      const result = await this.createNewProductUseCase.exec(body);
 
       return {
         statusCode: 200,
-        body: {
-          product: {
-            id: result.product.id,
-            name: result.product.name,
-            categoryId: result.category || [],
-            supplierId: result.supplierId || [],
-            image: result.product.image,
-            description: result.product.description,
-            stockQuantity: result.product.stockQuantity,
-            unitPrice: Number(result.product.unitPrice),
-            positionInStock: result.product.positionInStock,
-          },
-        },
+        body: JSON.stringify(result),
       };
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: {
-          message: 'Internal Server Error',
-        },
-      };
+      console.log('Error', error);
+      throw new Error(error.message);
     }
   }
 }
