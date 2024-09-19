1b88b8a8fc1796fb5724bacbcf433a66d57d6521
pedroaaluz <pedroaluz137@gmail.com>
2024-09-18 23:06:50 -0300

    Refactor logging in ListProducts lambda and update error handling in httpResponseStringify middleware

--- src/lambdas/ListProducts/index.ts
+++ src/lambdas/ListProducts/index.ts
@@ -32,6 +32,6 @@ const fn: HttpFn<
 export const bootstrap = handler(fn, [
   httpEventNormalizer(),
   zodValidatorMiddleware({requestSchema, responseSchema}),
-  httpErrorHandler({logger: console.error}),
+  //httpErrorHandler({logger: console.error}),
   httpResponseStringify(),
 ]);
